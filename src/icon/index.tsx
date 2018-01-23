430821c80f519b5fcef9950fb7efe7d07d9dfef8
Jon Whitehead <jsiwhitehead@gmail.com>
2018-01-23 19:18:07 +0000

    Improved icon component flexibility

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,29 +1,35 @@
 import * as React from 'react';
 
-export interface IconProps {
+export interface IconProps extends React.HTMLProps<{}> {
   path?: string;
   viewBox?: string;
   style?: React.CSSProperties;
 }
-export default function Icon({ path, viewBox, style }: IconProps) {
+export default function Icon({ path, viewBox, style, ...props }: IconProps) {
   const size = (style && (style.width || style.fontSize)) || 16;
   if (!path) {
     return (
       <span
-        style={{ display: 'block', width: size, height: size }}
+        style={{ ...style, display: 'block', width: size, height: size }}
         className="e5 e6 e7 e8 e9"
+        {...props}
       />
     );
   }
   return (
-    <svg
-      width={size}
-      height={size}
-      style={{ display: 'block' }}
-      viewBox={viewBox}
+    <span
+      style={{ ...style, display: 'block', width: 'auto', height: 'auto' }}
       className="e5 e6 e7 e8 e9"
+      {...props}
     >
-      <path style={{ fill: (style && style.color) || 'black' }} d={path} />
-    </svg>
+      <svg
+        width={size}
+        height={size}
+        style={{ display: 'block' }}
+        viewBox={viewBox}
+      >
+        <path style={{ fill: (style && style.color) || 'black' }} d={path} />
+      </svg>
+    </span>
   );
 }
